58fc50f334434d7df1f3d700d338fa2a3ffe28e0
mustapha335 <reezy77jay@gmail.com>
2022-07-30 21:28:32 +0100

    about section completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
-
+import About from "./components/About/About";
+// import Navbar from "./components/NavBar/Navbar";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className='App'>
       <Home />
+      {/* <Navbar /> */}
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className='about-container'>
+      <div className='about-wrapper'>
+        <div className='left'>
+          <div className='about-title'>
+            <h2 className='underline--blue'>Who Am I?</h2>
+          </div>
+          <h2 className='about-desc'>
+            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
+            aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
+            quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
+            fugiat sit in iste officiis commodi quidem hic quas.
+          </h2>
+        </div>
+        <div className='right'>
+          <div className='imagebg'></div>
+          <img
+            src='https://images.unsplash.com/photo-1610478865542-7d239b415687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGRhcmslMjBwcm9maWxlJTIwcGljdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+            alt=''
+            className='about-img'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
